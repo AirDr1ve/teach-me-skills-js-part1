edbf8ee0b298d8c237a9adcd91950fe859951af8
AirDr1ve <62071976+AirDr1ve@users.noreply.github.com>
2020-05-26 18:32:15 +0300

    hw-4

--- src/hw-4.js
+++ src/hw-4.js
@@ -3,17 +3,17 @@
  * Фибоначчи, длина массива будет приходить, как аргумент функции
  */
 function fibonacci(length) {
-    let sum = 0;
-    let arr = [];
+  let sum = 0;
+  let arr = [];
 
-    for (let i = 0; i < length; i++) {
-        if (arr.length > 1) sum += arr[arr.length - 2];
-        if (arr.length === 1) sum = 1;
-        if (arr.length === 0) sum = 0;
-        arr.push(sum);
-    }
+  for (let i = 0; i < length; i++) {
+    if (arr.length > 1) sum += arr[arr.length - 2];
+    if (arr.length === 1) sum = 1;
+    if (arr.length === 0) sum = 0;
+    arr.push(sum);
+  }
 
-    console.log(arr);
+  console.log(arr);
 }
 
 fibonacci(19)
@@ -22,28 +22,28 @@ fibonacci(19)
 /**
  * Написать свою функцию map
  */
-function map(array, callback){
-    let arrayResault = [];
-    for ( let i = 0; i < array.length; i++) {
-      arrayResault[i] = callback(array[i]);
-    }
-    return arrayResault
+function map(array, callback) {
+  let arrayResault = [];
+  for (let i = 0; i < array.length; i++) {
+    arrayResault[i] = callback(array[i]);
   }
-   console.log(map(['if', 'whay', 'house', 'street'], function(len){
-     return len.length
-   }));
-  
-   console.log(map([2, 10, 14, 24, 4], function(del){
-     return del/2
-  
-   }));
+  return arrayResault
+}
+console.log(map(['if', 'whay', 'house', 'street'], function (len) {
+  return len.length
+}));
+
+console.log(map([2, 10, 14, 24, 4], function (del) {
+  return del / 2
+
+}));
 
 /**
  * Написать свою функцию filter
  */
 function filter(array, callback) {
-    let arrayResult = [];
-    
+  let arrayResult = [];
+
 }
 
 /**
@@ -51,8 +51,8 @@ function filter(array, callback) {
  */
 function indexOf(array, value, fromIndex) {
 
-    let result = array.indexOf(3)
-    return result
+  let result = array.indexOf(3)
+  return result
 }
 
 
@@ -62,15 +62,32 @@ console.log(indexOf([2, 6, 8, 6, 37, 3, 5]));
  * Написать свою функцию reduce
  */
 function reduce(array, callback, acc) {
-
+  for (let i = 0; i < array.length; i++) {
+    acc = callback(acc, array[i]);
+  }
+  return acc
 }
+
+console.log(reduce(['M', '1', 'N', 'S', 'K'], function(acc, value){
+ return acc = acc + value;
+}, ''));
 
 /**
  * Написать свою функцию find
  */
 function find(array, callback) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return 'число ' + array[i] + ' есть'
+    }
+  }
 }
+
+console.log(find([10, 2, 3, 6], function (fin) {
+  if (fin === 10) {
+    return true
+  }
+}));
 
 /**
  * Написать свою функцию sort
@@ -83,12 +100,25 @@ function sort(array, callback) {
  * Написать свою функцию Object.keys
  */
 function objectKeys(obj) {
-
+  let array = [];
+ for (let key in obj) {
+   array.push(key);
+ }
+ return array
 }
+
+console.log(objectKeys({number: 1, name: 'Ilya', age: 27}));
 
 /**
  * Написать свою функцию Object.values
  */
-function objectValues(obj) {
+function objectValues(obj){
+  let array = [];
+  for (let key in obj){
+    array.push(obj[key]);
+  }
+  return array
 
 }
+
+console.log(objectValues({number: 1, name: 'Ilya', age: 27}));
